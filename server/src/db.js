f7437561150b7cce1510c0e0b86d98e0e746fa3f
daprionil <davidarenaoflol23@gmail.com>
2023-06-17 23:14:53 -0500

    fix(src/db.js): run model functions by reading the models directory

--- server/src/db.js
+++ server/src/db.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const {DB_USER, DB_NAME, DB_PORT, DB_HOST, DB_PASSWORD} = process.env;
 const {Sequelize} = require('sequelize');
 
@@ -5,8 +7,13 @@ const gistDb = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_
 
 const db = new Sequelize.Sequelize(gistDb,{logging:false});
 
-//Models
-//Relations
+//! Execute all models in './src/models/' with db 
+const pathModels = path.join(__dirname + '/models/');
+
+fs.readdirSync(pathModels).forEach((file) => {
+    const pathFile = pathModels + file
+    require(pathFile)(db);
+});
 
 module.exports = {
     db,
