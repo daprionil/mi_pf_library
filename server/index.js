01566fa0ddad7ed93b7cf5718e953551ca3f03b2
daprionil <davidarenaoflol23@gmail.com>
2023-06-17 23:22:35 -0500

    fix(index, src/routes root): WIP and write features to server execute
    fix(index, src/routes root): WIP and write features to server execute

--- server/index.js
+++ server/index.js
@@ -13,15 +13,15 @@ const {db} = require('./src/db.js');
 //* Get data in local
 const server = require('./src/server.js');
 const rootRouter = require('./src/routes/rootRouter.js');
-const port = process.env.PORT || 4001;
 const generateIdUUID = require('./src/utils/generateIdUUID.js');
+const port = process.env.PORT || 4001;
+
 
 //! Middlewares
 server.use(morgan('dev'));
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }))
-server.use(cors());
-
+server.use(cors()); 
 
 //* Routes
 server.use('/',rootRouter);
@@ -30,12 +30,59 @@ server.use('/',rootRouter);
 (async function main(){
     try {
         //* try conect database
-        await db.sync({alter:true});
+        await db.sync({force:true});
         console.log('The connection to the Database is successful');
 
         //* Start server if the database conection was successful
-        server.listen(port,() => {
+        server.listen(port,async () => {
             console.log(`Servidor corriendo en el puerto: ${port}`);
+
+            //Prueba
+            const uid = generateIdUUID();
+            const uid2 = generateIdUUID();
+
+            const date = new Date();
+            const datePublication = date.toISOString();
+            const book = {
+                IdBook: uid2,
+                title:"El camino del ser",
+                description:'EL mero exotiqueo',
+                writers:['Jordan Peterson'],
+                datePublication,
+                countryPublication: 1,
+                publisher: 'El Columpio',
+                language:1,
+                rate: 4.1,
+                numberPages:1,
+                reviews:[uid],
+                sellings:[1],
+                categories:[1]
+            };
+
+            await db.models.Country.create({
+                country:"Colombia"
+            });
+            await db.models.Category.create({
+                category:"Inpetu"
+            });
+
+            await db.models.Language.create({
+                language:"Español"
+            });
+            
+            await db.models.Review.create({
+                IdReview: uid,
+                IdBook: uid2,
+                rate: 4.0,
+                description:'El exotiqueo masimo'
+            });
+
+            //* Create one book
+            const bookCreated = await db.models.Book.create(book);
+            bookCreated.addCategory(book.categories);
+            bookCreated.addReview(book.reviews);
+
+            console.log(bookCreated.dataValues);
         });
     } catch ({message}) {
         console.log(message);
--- server/src/routes/rootRouter.js
+++ server/src/routes/rootRouter.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const { Book, Country, Category, Language, Review } = require('../db.js');
 
 const rootRouter = express.Router();
 
-rootRouter.get('/', (req,res) => {
-    res.send('Está correcto');
+rootRouter.get('/', async (req,res) => {
+    //Get all Books
+    const books = await Book.findAll({
+        include:[{
+            model:Country,
+            as:'countryPublicationData'
+        },{
+            model:Category,
+            through:{
+                attributes:[]
+            }
+        },{
+            model:Language,
+            as:'languageBook',
+            attributes:['language']
+        },{
+            model: Review,
+            through:{
+                attributes:[]
+            }
+        }],
+        attributes:{
+            exclude:['countryPublication','language']
+        }
+    },{});
+
+    const book = books[0].dataValues;
+    res.json(book);
 });
 
 module.exports = rootRouter;
