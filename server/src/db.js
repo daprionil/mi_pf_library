82497bb6221f8255b855fd9ebdf46f8fcdf50ecd
daprionil <lasparodias60@gmail.com>
2023-06-20 06:50:47 -0500

    fix(src/db.js_relationsModels): order to execute files in models

--- server/src/db.js
+++ server/src/db.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const path = require('path');
-const {DB_USER, DB_NAME, DB_PORT, DB_HOST, DB_PASSWORD} = process.env;
 const {Sequelize} = require('sequelize');
+
+const relationsModels = require('./models/relationsModels');
+const {DB_USER, DB_NAME, DB_PORT, DB_HOST, DB_PASSWORD} = process.env;
 
 const gistDb = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
@@ -11,9 +13,15 @@ const db = new Sequelize.Sequelize(gistDb,{logging:false});
 const pathModels = path.join(__dirname + '/models/');
 
 fs.readdirSync(pathModels).forEach((file) => {
+    ///? exclude file relations by execute
+    if(file === 'relationsModels.js') return;
+
     const pathFile = pathModels + file
     require(pathFile)(db);
 });
+
+//? execute relations
+relationsModels(db);
 
 module.exports = {
     db,
